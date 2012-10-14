83f7ea2b91fc81793ea775b3dbc67e86573613c5
Kriss Watt <kriss.watt@gmail.com>
2012-10-14 18:29:58 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request, session, render_template, make_response
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'Artoo says that the chances of survival are 725 to 1'
+
+
+@app.route('/')
+def dashboard():
+	return ''
+
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
